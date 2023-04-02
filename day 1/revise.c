5a0e4f0053dc5863d6bc2dfcc108a1ad8cef33cb
Anant <anant312004@gmail.com>
2023-04-02 22:04:43 +0530

    Day 1

--- day 1/revise.c
+++ day 1/revise.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 
+int check(int n);
+
 int main(void){
-    printf("Hello I'm back\n");
+    // normal stuff with loop.
+    int value;
+    value = 0;
+    for (int i = 0; i<5; i++){
+        value ++;
+    }
+    
+    // using while loop.
+    int output = check(value);
+    if (output != 1){
+        printf("Value isn't below 10\n");
+    }
+    else{
+        printf("Yes you are welcomed.\n");
+    }
+
+    printf("Value of value variable is %i\n", value);
+    char* name;
+    name = "anant";
+    printf("Hello I'm %s, I'm back\n", name);
+}
+
+
+int check(int n){
+    // This function checks that given integer is bigger than 10 or not.
+
+    if (n < 10){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
