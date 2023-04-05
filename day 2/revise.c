5a8e8792b3037bfa4d9111b893a5cb5556b97bb8
Anant <anant312004@gmail.com>
2023-04-05 10:11:32 +0530

    Added day 2

--- day 2/revise.c
+++ day 2/revise.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int main(void){
     // while loop usage
@@ -14,4 +16,21 @@ int main(void){
         coint++;
     }
     while(coint<10);
+    
+    // Arrays
+    int ages[] = {50, 60, 70};
+    char* names[] = {"anant", "shiv", "hariom"};
+    char characters[] = {'a', 'b', 'c'};  
+    char* name = "AnAnT";           // How strings are made in C
+    printf("Hello %s\n", name); 
+
+    printf("Hello %s %i %c %i\n", names[2], ages[2], characters[0], characters[0]);
+
+    printf("%c\n",toupper(name[1])); // lower & upper conversion of char.
+    printf("%i\n", name[5]); // Implicit and explicit conversion of char.
+
+    for(int i = 0; i < strlen(name); i++){
+        printf("%c",tolower(name[i]));
+    }
+    printf("\n");
 }
